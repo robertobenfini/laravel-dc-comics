80a1ae8af8245e571aa652aa232e61980f2f8051
robertobenfini <roberto.benfini@gmail.com>
2023-08-01 16:20:08 +0200

    [ADD] value imput form edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -12,21 +12,21 @@
                         <div class="row">
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Titolo</label>
-                                <input type="text" name="title" id="title" placeholder="Inserisci il titolo" class="form-control">
+                                <input type="text" name="title" id="title" placeholder="Inserisci il titolo" class="form-control" value="{{ $comic->title }}">
                             </div>
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Descrizione</label>
-                                <input type="textarea" name="description" id="description" placeholder="Inserisci la descrizione" class="form-control">
+                                <input type="textarea" name="description" id="description" placeholder="Inserisci la descrizione" class="form-control" value="{{ $comic->description }}">
                             </div>
 
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Thumb</label>
-                                <input type="url" name="thumb" id="thumb" placeholder="Inserisci la copertina" class="form-control">
+                                <input type="url" name="thumb" id="thumb" placeholder="Inserisci la copertina" class="form-control" value="{{ $comic->thumb }}">
                             </div>
 
                             <div class="col-12 my-3">
                                 <label class="control-label my-3">Titolo</label>
-                                <input type="text" name="series" id="series" placeholder="Inserisci il titolo" class="form-control">
+                                <input type="text" name="series" id="series" placeholder="Inserisci il titolo" class="form-control" value="{{ $comic->series }}">
                             </div>
 
                             <button type="submit" class="btn btn-success">Salva</button>
